4b05a68bab7b0848fbdb9f5f24a4f80cac91ed5a
simon <simon@glashoff.dk>
2017-03-24 13:34:56 +0100

    Begun reworking Kruskal

--- PrimsTreesKruskalTrees/PrimsTreesKruskalTrees/Kruskal.cs
+++ PrimsTreesKruskalTrees/PrimsTreesKruskalTrees/Kruskal.cs
@@ -9,114 +9,115 @@ namespace PrimsTreesKruskalTrees
     
     class Kruskal
     {
-        List<int> vistedlocations = new List<int>();
-        List<int> connectionTracker = new List<int>();
-        List<int> edgesOfGraph = new List<int>();
-        int[,] matrix;
-        public Kruskal()
+
+        List<Node> nodeList = new List<Node>();
+
+        List<Node> visitedlocations = new List<Node>();
+        List<Node> connectionTracker = new List<Node>();
+        List<Edge> edgesOfGraph = new List<Edge>();
+
+        public void AddNode(string name)
         {
-            if (matrix == null)
+            Node node = new Node(name, nodeList.Count);
+            nodeList.Add(node);
+        }
+        public void AddEdgeToNode(string from, string to, int weigth)
+        {
+            foreach (Node fromNode in nodeList)
             {
-                matrix = new int[9, 9];
-                for (int i = 0; i < 9; i++)
+                if (from == fromNode.name)
                 {
-                    for (int j = 0; j < 9; j++)
+                    foreach (Node toNode in nodeList)
                     {
-                        matrix[i, j] = -1;
+                        if (to == toNode.name)
+                        {
+                            Edge newEdge = new Edge(fromNode, toNode, weigth);
+                            fromNode.edges.Add(newEdge);
+                        }
                     }
                 }
             }
-        }
-        public void DirectionalFillMatrix()
-        {
-            matrix[0, 2] = 67;
-            matrix[0, 3] = 33;
-            matrix[0, 7] = 49;
-            matrix[1, 0] = 90;
-            matrix[1, 5] = 56;
-            matrix[1, 6] = 45;
-            matrix[2, 8] = 61;
-            matrix[4, 0] = 22;
-            matrix[5, 8] = 43;
-            matrix[6, 0] = 58;
-            matrix[7, 4] = 67;
-            matrix[7, 2] = 38;
-            matrix[8, 0] = 59;
-        }
-        public void NonDirectionalFillMatrix()
-        {
-            DirectionalFillMatrix();
-            matrix[2, 0] = 67;
-            matrix[3, 0] = 33;
-            matrix[7, 0] = 49;
-            matrix[0, 1] = 90;
-            matrix[5, 1] = 56;
-            matrix[6, 1] = 45;
-            matrix[8, 2] = 61;
-            matrix[0, 4] = 22;
-            matrix[8, 5] = 43;
-            matrix[0, 6] = 58;
-            matrix[4, 7] = 67;
-            matrix[2, 7] = 38;
-            matrix[0, 8] = 59;
+
         }
         public void CLear()
         {
-            vistedlocations.Clear();
-            //visited = new bool[9];
+            visitedlocations.Clear();
         }
         public void Print()
         {
-            int k = 0;
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < nodeList.Count; i++)
             {
-                for (int j = 0; j < 9; j++)
+                Console.WriteLine(" ");
+                Console.WriteLine(nodeList[i]);
+                for (int j = 0; j < nodeList[i].edges.Count; j++)
                 {
-                    if (k < 9)
-                    {
-                        Console.Write(matrix[i, j] + " , ");
-                        k++;
-                    }
-                    else
-                    {
-                        k = 0;
-                        Console.Write("\n");
-                        Console.Write(matrix[i, j] + " , ");
-                        k++;
-                    }
-
+                    Console.WriteLine("Can see: " + nodeList[i].edges[j]);
                 }
             }
         }
-        public List<int> KruskalTraverse()
+        public List<Node> KruskalStart()
         {
-            int graph = 0;
-            foreach (int location in matrix) //laver en liste med hver location for at holde styr på hvilke graph grupper de er i.
+            foreach (Node city in nodeList) //Goes through all nodes
             {
-                connectionTracker.Add(graph); //Hver index er en location, på hver punkt står hvilken gruppe de er i.
-                graph++; //de starter i hver sin gruppe
-
-            }
-            for (int x = 0; x < matrix.Length; x++)
-            {
-                for (int y = 0; y < matrix.Length; y++)
+                foreach (Edge edge in city.edges) //For every node goes through their edges
                 {
-                    edgesOfGraph.Add(matrix[x, y]); //finder alle kanterne og lægger dem i en liste.
+                    if (!edgesOfGraph.Contains(edge)) //If the edge is not already in the list
+                    {
+                        edgesOfGraph.Add(edge); //Add the edge to the list of edges
+                    }
                 }
+
             }
-            edgesOfGraph.Sort();//sortere den liste bagefter.
-
-            foreach (int edge in edgesOfGraph) // for hver kant i vores graph
-            {
-                //få de locations den binder sammens navne
-                //check om de allerede er i samme graph set
-                //hvis ikke kombiner de 2 set, og tilføj edgens locations til listen
- 
-            }
-
-
-            return vistedlocations;
+            edgesOfGraph.Sort((x, y) => x.weigth.CompareTo(y.weigth)); //Sorts the list in ascending order
+            return KruskalTraverse();
         }
+        private List<Node> KruskalTraverse()
+        {
+            
+            return visitedlocations;
+        }
+
+
+
+
+
+
+
+
+        //List<int> vistedlocations = new List<int>();
+        //List<int> connectionTracker = new List<int>();
+        //List<int> edgesOfGraph = new List<int>();
+        //int[,] matrix;
         
+        //public List<int> KruskalTraverse()
+        //{
+        //    int graph = 0;
+        //    foreach (int location in matrix) //laver en liste med hver location for at holde styr på hvilke graph grupper de er i.
+        //    {
+        //        connectionTracker.Add(graph); //Hver index er en location, på hver punkt står hvilken gruppe de er i.
+        //        graph++; //de starter i hver sin gruppe
+
+        //    }
+        //    for (int x = 0; x < matrix.Length; x++)
+        //    {
+        //        for (int y = 0; y < matrix.Length; y++)
+        //        {
+        //            edgesOfGraph.Add(matrix[x, y]); //finder alle kanterne og lægger dem i en liste.
+        //        }
+        //    }
+        //    edgesOfGraph.Sort();//sortere den liste bagefter.
+
+        //    foreach (int edge in edgesOfGraph) // for hver kant i vores graph
+        //    {
+        //        //få de locations den binder sammens navne
+        //        //check om de allerede er i samme graph set
+        //        //hvis ikke kombiner de 2 set, og tilføj edgens locations til listen
+
+        //    }
+
+
+        //    return vistedlocations;
+        //}
+
     }
 }
--- PrimsTreesKruskalTrees/PrimsTreesKruskalTrees/Program.cs
+++ PrimsTreesKruskalTrees/PrimsTreesKruskalTrees/Program.cs
@@ -29,13 +29,13 @@ namespace PrimsTreesKruskalTrees
         }
         public void Run()
         {
-            TestPrims();
-            //TestKruskal();
+            //TestPrims();
+            TestKruskal();
         }
         public void TestPrims()
         {
-            //DirectedFillPrims();
-            NonDirectedFillPrims();
+            DirectedFillPrims();
+            //NonDirectedFillPrims();
             foreach (Node city in prims.PrimsStart("Odense"))
             {
                 Console.WriteLine(city);
@@ -108,22 +108,82 @@ namespace PrimsTreesKruskalTrees
             prims.AddEdgeToNode("Otterup", "Bogense", 53);
             prims.AddEdgeToNode("Odense", "Otterup", 22);
         }
-        //public void TestKruskal()
-        //{
-        //    kruskal.DirectionalFillMatrix();
-        //    kruskal.NonDirectionalFillMatrix();
+        public void TestKruskal()
+        {
+            DirectedFillKruskal();
+            //NonDirectedFillKruskal();
+            foreach (Node city in kruskal.KruskalStart())
+            {
+                Console.WriteLine(city);
+            }
+            Console.ReadKey();
+        }
+        public void DirectedFillKruskal()
+        {
+            kruskal.AddNode("Odense");
+            kruskal.AddNode("Svendborg");
+            kruskal.AddNode("Middelfart");
+            kruskal.AddNode("Kerteminde");
+            kruskal.AddNode("Otterup");
+            kruskal.AddNode("Ringe");
+            kruskal.AddNode("Nyborg");
+            kruskal.AddNode("Bogense");
+            kruskal.AddNode("Assens");
 
-        //    foreach (int item in kruskal.KruskalTraverse()
-        //    {
-        //        Console.WriteLine((cities)item);
-        //    }
-        //    Console.ReadKey();
-        //    foreach (string item in prims.rute)
-        //    {
-        //        Console.WriteLine(item);
-        //    }
-        //    Console.ReadKey();
-        //}
+            kruskal.AddEdgeToNode("Odense", "Bogense", 49);
+            kruskal.AddEdgeToNode("Bogense", "Middelfart", 38);
+            kruskal.AddEdgeToNode("Middelfart", "Assens", 61);
+            kruskal.AddEdgeToNode("Odense", "Middelfart", 67);
+            kruskal.AddEdgeToNode("Assens", "Odense", 59);
+            kruskal.AddEdgeToNode("Ringe", "Assens", 43);
+            kruskal.AddEdgeToNode("Svendborg", "Ringe", 56);
+            kruskal.AddEdgeToNode("Svendborg", "Odense", 90);
+            kruskal.AddEdgeToNode("Svendborg", "Nyborg", 45);
+            kruskal.AddEdgeToNode("Nyborg", "Odense", 58);
+            kruskal.AddEdgeToNode("Odense", "Kerteminde", 33);
+            kruskal.AddEdgeToNode("Bogense", "Otterup", 53);
+            kruskal.AddEdgeToNode("Otterup", "Odense", 22);
+        }
+        public void NonDirectedFillKruskal()
+        {
+            kruskal.AddNode("Odense");
+            kruskal.AddNode("Svendborg");
+            kruskal.AddNode("Middelfart");
+            kruskal.AddNode("Kerteminde");
+            kruskal.AddNode("Otterup");
+            kruskal.AddNode("Ringe");
+            kruskal.AddNode("Nyborg");
+            kruskal.AddNode("Bogense");
+            kruskal.AddNode("Assens");
+
+            kruskal.AddEdgeToNode("Odense", " Bogense", 49);
+            kruskal.AddEdgeToNode("Bogense", "Middelfart", 38);
+            kruskal.AddEdgeToNode("Middelfart", "Assens", 61);
+            kruskal.AddEdgeToNode("Odense", "Middelfart", 67);
+            kruskal.AddEdgeToNode("Assens", "Odense", 59);
+            kruskal.AddEdgeToNode("Ringe", "Assens", 43);
+            kruskal.AddEdgeToNode("Svendborg", "Ringe", 56);
+            kruskal.AddEdgeToNode("Svendborg", "Odense", 90);
+            kruskal.AddEdgeToNode("Svendborg", "Nyborg", 45);
+            kruskal.AddEdgeToNode("Nyborg", "Odense", 58);
+            kruskal.AddEdgeToNode("Odense", "Kerteminde", 33);
+            kruskal.AddEdgeToNode("Bogense", "Otterup", 53);
+            kruskal.AddEdgeToNode("Otterup", "Odense", 22);
+
+            kruskal.AddEdgeToNode("Bogense", " Odense", 49);
+            kruskal.AddEdgeToNode("Middelfart", "Bogense", 38);
+            kruskal.AddEdgeToNode("Assens", "Middelfart", 61);
+            kruskal.AddEdgeToNode("Middelfart", "Odense", 67);
+            kruskal.AddEdgeToNode("Odense", "Assens", 59);
+            kruskal.AddEdgeToNode("Assens", "Ringe", 43);
+            kruskal.AddEdgeToNode("Ringe", "Svendborg", 56);
+            kruskal.AddEdgeToNode("Odense", "Svendborg", 90);
+            kruskal.AddEdgeToNode("Nyborg", "Svendborg", 45);
+            kruskal.AddEdgeToNode("Odense", "Nyborg", 58);
+            kruskal.AddEdgeToNode("Kerteminde", "Odense", 33);
+            kruskal.AddEdgeToNode("Otterup", "Bogense", 53);
+            kruskal.AddEdgeToNode("Odense", "Otterup", 22);
+        }
     }
     
 }
